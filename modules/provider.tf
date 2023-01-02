f9c2c23452b35fccac47d12c431c5b99f7dd2111
oludare odo <darekorex143@gmail.com>
2023-01-02 16:11:21 -0600

    waf code

--- modules/provider.tf
+++ modules/provider.tf
@@ -1,16 +1,3 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 terraform {
   backend "s3" {}
 }
-
-#terraform {
-# required_providers {
-#  aws = {
-#   source  = "hashicorp/aws"
-#  version = ">=3.0.0, < 5.0.0"
-#}
-#}
-#}
